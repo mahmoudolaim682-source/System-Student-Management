30af08147c0d80672f4ac9ee605ad2971ba5fae4
Mahmoud <mahmoudolaim682@gmail.com>
2026-03-03 00:29:42 +0200

    Add Student class (by Lobna Mohamed)

---
+++ Student.java
@@ -0,0 +1,32 @@
+public class Student {
+
+    private int id;
+    private String name;
+    private double grade;
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+        this.grade = 0.0;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getGrade() {
+        return grade;
+    }
+
+    public void setGrade(double grade) {
+        this.grade = grade;
+    }
+
+    public void displayInfo() {
+        System.out.println("ID: " + id + " | Name: " + name + " | Grade: " + grade);
+    }
+}
