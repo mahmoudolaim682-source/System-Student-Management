b228500587ad4bd1384292274a0ee732408422f3
Mahmoud <mahmoudolaim682@gmail.com>
2026-03-03 00:29:56 +0200

    Add Course class (by Lobna Mohamed)

---
+++ Course.java
@@ -0,0 +1,23 @@
+public class Course {
+
+    private String courseCode;
+    private String courseName;
+
+    public Course(String courseCode, String courseName) {
+        this.courseCode = courseCode;
+        this.courseName = courseName;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void displayCourseInfo() {
+        System.out.println("Course Code: " + courseCode +
+                " | Course Name: " + courseName);
+    }
+}
